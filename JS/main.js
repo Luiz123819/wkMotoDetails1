d68e7ddffa54f0a9fbc340880bd3e1e2ad594143
Luiz Fernando <138504481+Luiz123819@users.noreply.github.com>
2026-01-26 16:02:04 -0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -29,26 +29,49 @@ overlay.addEventListener('click', () => {
 });
 
 
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      320: { slidesPerView: 1 },
-      768: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 }
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
+  }
+});
+
+const btn = document.querySelector(".verMaisBTN"); const hiddenLine = document.querySelector(".lineHidden"); btn.addEventListener("click", function () { hiddenLine.classList.toggle("show"); if (hiddenLine.classList.contains("show")) { btn.textContent = "Ver Menos"; } else { btn.textContent = "Ver Mais"; } });
+
+
+
+const hearts = document.querySelectorAll(".fotoCard i");
+
+hearts.forEach((heart, index) => {
+  // Verifica se já estava salvo como ativo
+  if (localStorage.getItem("heart-" + index) === "active") {
+    heart.classList.add("active");
+  }
+
+  // Corrigido: evento no próprio coração
+  heart.addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    if (this.classList.contains("active")) {
+      localStorage.setItem("heart-" + index, "active");
+    } else {
+      localStorage.removeItem("heart-" + index);
     }
   });
-
+});
